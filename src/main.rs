ce92fddafad337d74ae39b095e584d1359039e59
Martin Kim Dung-Pham <martinkim.pham@gmail.com>
2021-04-18 20:53:05 +0200

    Go back and forward depending on button press or optical sensor

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use atmega168_hal::prelude::*;
 
 #[atmega168_hal::entry]
 fn main() -> ! {
+    let mut step: i8 = 32;
+
     let dp = atmega168_hal::pac::Peripherals::take().unwrap();
     let mut port_b = dp.PORTB.split();
     let mut port_c = dp.PORTC.split();
@@ -29,112 +31,59 @@ fn main() -> ! {
     let mut c2 = port_c.pc2.into_output(&mut port_c.ddr);
     let mut c3 = port_c.pc3.into_output(&mut port_c.ddr);
     let mut c4 = port_c.pc4.into_output(&mut port_c.ddr);
+
     let mut delay = Delay::<MHz1>::new();
 
     loop {
         let value: u16 = nb::block!(adc.read(&mut optocoupler)).void_unwrap();
-        if value > 5 || button_1.is_high().unwrap_or(false) {
+        if value > 5 {
             led.set_high().void_unwrap();
         } else {
             led.set_low().void_unwrap();
         }
 
         if button_1.is_high().unwrap_or(false) {
-            c1.set_high().void_unwrap();
-            c2.set_low().void_unwrap();
-            c3.set_high().void_unwrap();
-            c4.set_low().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_low().void_unwrap();
-            c2.set_low().void_unwrap();
-            c3.set_high().void_unwrap();
-            c4.set_low().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_low().void_unwrap();
-            c2.set_high().void_unwrap();
-            c3.set_high().void_unwrap();
-            c4.set_low().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_low().void_unwrap();
-            c2.set_high().void_unwrap();
-            c3.set_low().void_unwrap();
-            c4.set_low().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_low().void_unwrap();
-            c2.set_high().void_unwrap();
-            c3.set_low().void_unwrap();
-            c4.set_high().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_low().void_unwrap();
-            c2.set_low().void_unwrap();
-            c3.set_low().void_unwrap();
-            c4.set_high().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_high().void_unwrap();
-            c2.set_low().void_unwrap();
-            c3.set_low().void_unwrap();
-            c4.set_high().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_high().void_unwrap();
-            c2.set_low().void_unwrap();
-            c3.set_low().void_unwrap();
-            c4.set_low().void_unwrap();
-            delay.delay_ms(5u8);
-        } else {
-            c1.set_high().void_unwrap();
-            c2.set_low().void_unwrap();
-            c3.set_high().void_unwrap();
-            c4.set_low().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_low().void_unwrap();
-            c2.set_low().void_unwrap();
-            c3.set_high().void_unwrap();
-            c4.set_low().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_low().void_unwrap();
-            c2.set_high().void_unwrap();
-            c3.set_high().void_unwrap();
-            c4.set_low().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_low().void_unwrap();
-            c2.set_high().void_unwrap();
-            c3.set_low().void_unwrap();
-            c4.set_low().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_low().void_unwrap();
-            c2.set_high().void_unwrap();
-            c3.set_low().void_unwrap();
-            c4.set_high().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_low().void_unwrap();
-            c2.set_low().void_unwrap();
-            c3.set_low().void_unwrap();
-            c4.set_high().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_high().void_unwrap();
-            c2.set_low().void_unwrap();
-            c3.set_low().void_unwrap();
-            c4.set_high().void_unwrap();
-            delay.delay_ms(5u8);
-
-            c1.set_high().void_unwrap();
-            c2.set_low().void_unwrap();
-            c3.set_low().void_unwrap();
-            c4.set_low().void_unwrap();
-            delay.delay_ms(5u8);
+            led.set_high().void_unwrap();
         }
+
+        if value > 5 || button_1.is_high().unwrap_or(false) {
+            step += 1;
+            step %= 4;
+        } else {
+            step -= 1;
+            if step <= 0 {
+                step = 4
+            }
+        }
+
+        match step % 4 {
+            0 => {
+                c1.set_high().void_unwrap();
+                c2.set_low().void_unwrap();
+                c3.set_high().void_unwrap();
+                c4.set_low().void_unwrap()
+            }
+            1 => {
+                c1.set_low().void_unwrap();
+                c2.set_high().void_unwrap();
+                c3.set_high().void_unwrap();
+                c4.set_low().void_unwrap()
+            }
+            2 => {
+                c1.set_low().void_unwrap();
+                c2.set_high().void_unwrap();
+                c3.set_low().void_unwrap();
+                c4.set_high().void_unwrap()
+            }
+            3 => {
+                c1.set_high().void_unwrap();
+                c2.set_low().void_unwrap();
+                c3.set_low().void_unwrap();
+                c4.set_high().void_unwrap()
+            }
+            _ => (),
+        }
+
+        delay.delay_ms(1u8);
     }
 }
