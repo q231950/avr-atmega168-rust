8e703925f394ab1223dfb58d5b9fc03af15d3bba
Martin Kim Dung-Pham <martinkim.pham@gmail.com>
2021-04-18 20:36:57 +0200

    Elevator first steps

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,140 @@
 #![no_std]
 #![no_main]
 
-// This uses an attiny in combination with a CD4026B to drive a 7 segment display.
-
 // Pull in the panic handler from panic-halt
 extern crate panic_halt;
 
+use atmega168_hal::clock::*;
+use atmega168_hal::delay::Delay;
 use atmega168_hal::prelude::*;
 
 #[atmega168_hal::entry]
 fn main() -> ! {
     let dp = atmega168_hal::pac::Peripherals::take().unwrap();
-    unimplemented!()
+    let mut port_b = dp.PORTB.split();
+    let mut port_c = dp.PORTC.split();
+    let mut port_d = dp.PORTD.split();
+    let mut adc = atmega168_hal::adc::Adc::new(dp.ADC, Default::default());
+
+    // Reflective Opto Coupler
+    let mut optocoupler = port_c.pc0.into_analog_input(&mut adc);
+    let mut led = port_b.pb1.into_output(&mut port_b.ddr);
+
+    // Push Button Switch
+    let button_1 = port_d.pd0.into_pull_up_input(&mut port_d.ddr);
+
+    // Stepper Motor
+
+    let mut c1 = port_c.pc1.into_output(&mut port_c.ddr);
+    let mut c2 = port_c.pc2.into_output(&mut port_c.ddr);
+    let mut c3 = port_c.pc3.into_output(&mut port_c.ddr);
+    let mut c4 = port_c.pc4.into_output(&mut port_c.ddr);
+    let mut delay = Delay::<MHz1>::new();
+
+    loop {
+        let value: u16 = nb::block!(adc.read(&mut optocoupler)).void_unwrap();
+        if value > 5 || button_1.is_high().unwrap_or(false) {
+            led.set_high().void_unwrap();
+        } else {
+            led.set_low().void_unwrap();
+        }
+
+        if button_1.is_high().unwrap_or(false) {
+            c1.set_high().void_unwrap();
+            c2.set_low().void_unwrap();
+            c3.set_high().void_unwrap();
+            c4.set_low().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_low().void_unwrap();
+            c2.set_low().void_unwrap();
+            c3.set_high().void_unwrap();
+            c4.set_low().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_low().void_unwrap();
+            c2.set_high().void_unwrap();
+            c3.set_high().void_unwrap();
+            c4.set_low().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_low().void_unwrap();
+            c2.set_high().void_unwrap();
+            c3.set_low().void_unwrap();
+            c4.set_low().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_low().void_unwrap();
+            c2.set_high().void_unwrap();
+            c3.set_low().void_unwrap();
+            c4.set_high().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_low().void_unwrap();
+            c2.set_low().void_unwrap();
+            c3.set_low().void_unwrap();
+            c4.set_high().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_high().void_unwrap();
+            c2.set_low().void_unwrap();
+            c3.set_low().void_unwrap();
+            c4.set_high().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_high().void_unwrap();
+            c2.set_low().void_unwrap();
+            c3.set_low().void_unwrap();
+            c4.set_low().void_unwrap();
+            delay.delay_ms(5u8);
+        } else {
+            c1.set_high().void_unwrap();
+            c2.set_low().void_unwrap();
+            c3.set_high().void_unwrap();
+            c4.set_low().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_low().void_unwrap();
+            c2.set_low().void_unwrap();
+            c3.set_high().void_unwrap();
+            c4.set_low().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_low().void_unwrap();
+            c2.set_high().void_unwrap();
+            c3.set_high().void_unwrap();
+            c4.set_low().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_low().void_unwrap();
+            c2.set_high().void_unwrap();
+            c3.set_low().void_unwrap();
+            c4.set_low().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_low().void_unwrap();
+            c2.set_high().void_unwrap();
+            c3.set_low().void_unwrap();
+            c4.set_high().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_low().void_unwrap();
+            c2.set_low().void_unwrap();
+            c3.set_low().void_unwrap();
+            c4.set_high().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_high().void_unwrap();
+            c2.set_low().void_unwrap();
+            c3.set_low().void_unwrap();
+            c4.set_high().void_unwrap();
+            delay.delay_ms(5u8);
+
+            c1.set_high().void_unwrap();
+            c2.set_low().void_unwrap();
+            c3.set_low().void_unwrap();
+            c4.set_low().void_unwrap();
+            delay.delay_ms(5u8);
+        }
+    }
 }
